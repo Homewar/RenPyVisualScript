3b7f76ade23f2f5d7a53c68e4eb5d767a285fbd8
Homewarriors <divaniyvoin1337@gmail.com>
2026-05-04 14:50:39 +0300

    Remove icon

--- RenPyVisualScriptMVVM/obj/Debug/net8.0/RenPyVisualScriptMVVM.AssemblyInfo.cs
+++ RenPyVisualScriptMVVM/obj/Debug/net8.0/RenPyVisualScriptMVVM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RenPyVisualScriptMVVM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab217660cd0522487e5c8d7569ec666fda35567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3b3c5e17610c273355414ebfb036a20ab05c0e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RenPyVisualScriptMVVM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RenPyVisualScriptMVVM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
